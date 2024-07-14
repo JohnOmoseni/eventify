07d75fca702aa7c472935f7af0a96709fa9f2e7d
John Omoseni <johnnyomoseni100@gmail.com>
2024-07-14 02:07:21 +0100

    feat: reinitialized clerk

--- src/app/(home)/events/create/page.tsx
+++ src/app/(home)/events/create/page.tsx
@@ -3,13 +3,17 @@ import EventForm from "../_sections/EventForm";
 
 async function CreateEvent() {
   // customize your session token
-  const { userId }: { userId: string | null } = auth();
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  // returns the clerkId
+  // const { userId }: { userId: string | null } = auth();
 
   console.log("UserID:", userId);
 
   return (
     <div className="px-3 pb-4 pt-6 sm:px-6">
-      <EventForm type="Create" userId={userId!} />
+      <EventForm type="Create" userId={userId} />
     </div>
   );
 }
